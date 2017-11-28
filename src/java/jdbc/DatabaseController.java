a303c250dce93f7e62ce35d62d89a736ad21dc4a
t4-wang <t4-wang@STU-CZC4012NB1.campus.ads.uwe.ac.uk>
2017-11-28 12:41:01 +0000

    1. added admin e,f,g,h functions. 2. added lumpsum related functions. 3. added optional automatic charge member fee (TBD).

--- src/java/jdbc/DatabaseController.java
+++ src/java/jdbc/DatabaseController.java
@@ -5,6 +5,7 @@
  */
 package jdbc;
 
+import java.math.RoundingMode;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.Statement;
@@ -15,6 +16,9 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Time;
 import static java.sql.Types.NULL;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,6 +33,11 @@ public class DatabaseController {
     Statement statement = null;
     ResultSet resultSet = null;
     int ClaimID = 4;
+    LocalDate now = LocalDate.now();
+    String startOfYear = now.with(TemporalAdjusters.firstDayOfYear()).toString();
+    String endOfYear = now.with(TemporalAdjusters.lastDayOfYear()).toString();
+    String today = now.toString();
+    DecimalFormat df = new DecimalFormat("#.##");
 
     public boolean NewClaim(String rationale, int amount) {
         PreparedStatement ps = null;
@@ -129,20 +138,182 @@ public class DatabaseController {
         select("select * from members where \"status\" like 'APPLIED'");
         return makeTable(rsToList());//results;
     }
-    
-    public Boolean processClaim(int id, String status){
-        
+
+    public Boolean processClaim(int id, String status) {
+
         Boolean processed = false;
-        String query = "update claims set \"status\" = '"+ status+"' where \"id\" ="+id;
-        
-        try{
+        String query = "update claims set \"status\" = '" + status + "' where \"id\" =" + id;
+
+        try {
             statement = connection.createStatement();
             statement.executeUpdate(query);
             processed = true;
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println("way way" + e);
-            //results = e.toString();
-        }        
-        return processed;//results;
+        }
+        return processed;
+    }
+
+    public Boolean chargeLumpsum() {
+
+        PreparedStatement ps = null;
+        Boolean claimed = false;
+        double fee = calcLumpsum();
+        String query = "UPDATE members SET balance = (balance+" + fee + ") WHERE status!='APPLIED'";
+
+        if (today.equals(endOfYear)) {
+            try {
+                ps = connection.prepareStatement(query);
+                ps.executeUpdate();
+                ps.close();
+                claimed = true;
+            } catch (SQLException s) {
+                System.out.println("SQL statement is not executed! " + s.getMessage());
+            }
+        }
+        return claimed;
+    }//method
+
+    public Boolean processApplication(int id, String status) {
+
+        Boolean processed = false;
+        PreparedStatement ps = null;
+        String query = "SELECT * FROM payments'";
+        String username;
+
+        try {
+            select(query);
+            while (resultSet.next() && processed == false) {
+                if (id == resultSet.getInt("id")) {
+                    username = resultSet.getString("mem_id");
+
+                    updateMembership(username, "APPROVED");
+                    updateBalance(username, -10);
+
+                    processed = true;
+                }
+            }
+        } catch (SQLException s) {
+            System.out.println("SQL statement is not executed! " + s.getMessage());
+
+        }
+        return processed;
+    }    
+
+    public Boolean updateMembership(String memid, String status) {
+
+        Boolean updated = false;
+        PreparedStatement ps = null;
+        String queryApprove = "UPDATE members SET status ='APPROVED', dor =DATE_ADD(dor, INTERVAL 1 YEAR) WHERE id='" + memid + "'";        
+        String querySuspend = "update members set \"status\" ='SUSPENDED' WHERE id='" + memid + "'";
+
+        try {
+            if (status.equals("APPROVED")) {
+                ps = connection.prepareStatement(queryApprove);
+                ps.executeUpdate();
+                ps.close();
+                updateBalance(memid, -10);
+                updated = true;
+
+            } else if (status.equals("SUSPENDED")) {
+                ps = connection.prepareStatement(querySuspend);
+                ps.executeUpdate();
+                ps.close();
+                updated = true;
+            }
+
+        } catch (SQLException s) {
+            System.out.println("SQL statement is not executed! " + s.getMessage());
+
+        }
+        return updated;
+    }
+
+//    public Boolean chargeFee(String id) {
+//
+//        Boolean charged = false;
+//        String query = "select * from members where \"id\"= '" + id + "' and \"status\" ='APPROVED' and \"dor\" <='" + today + "'";
+//
+//        try {
+//            select(query);
+//            while (resultSet.next()) {
+//                updateMembership(id, "SUSPENDED");
+//                updateBalance(id, 10);
+//                charged = true;
+//            }
+//        } catch (SQLException s) {
+//            System.out.println("SQL statement is not executed! " + s.getMessage());
+//        }
+//        return charged;
+//    }
+
+    private void updateBalance(String username, double amount) {
+
+        PreparedStatement ps = null;
+        String queryUpdate = "UPDATE members SET \"balance\" =(\"balance\"+" + amount + ") WHERE \"id\"='" + username + "'";
+
+        try {
+            ps = connection.prepareStatement(queryUpdate);
+            ps.executeUpdate();
+            ps.close();
+
+        } catch (SQLException s) {
+            System.out.println("SQL statement is not executed! " + s.getMessage());
+
+        }
+    }
+
+    public String listIncome() throws SQLException {
+        String results = "";
+        //Double income = 0.00;
+
+        select("select * from payments where \"date\" between '" + startOfYear + "' and '" + endOfYear + "'");
+
+        //while (resultSet.next()) {
+        //  income = income + resultSet.getDouble("amount");
+        //}
+        return makeTable(rsToList());//results;
+        //return income;
+    }//method
+
+    public String listExpense() throws SQLException {
+        String results = "";
+        //Double expense =0.00;
+
+        select("select * from claims where \"status\" ='APPROVED' and \"date\" between '" + startOfYear + "' and '" + endOfYear + "'");
+
+        //while (resultSet.next()) {
+        //  expense = expense + resultSet.getDouble("amount");
+        //}
+        return makeTable(rsToList());//results;
+        //return expense;
+    }//method
+
+    private double calcLumpsum() {
+
+        double fee = 0;
+        double total = 0;
+        double count = 0;
+
+        df.setRoundingMode(RoundingMode.FLOOR);
+        String queryCount = "select count(*) from members";
+        String querySum = "select sum(\"amount\") from claims where \"status\" ='APPROVED' and \"date\" between '" + startOfYear + "' and '" + endOfYear + "'";
+
+        try {
+            select(queryCount);
+            while (resultSet.next()) {
+                count = resultSet.getInt(1);
+            }
+            select(querySum);
+            while (resultSet.next()) {
+                total = resultSet.getDouble(1);
+            }
+
+            fee = new Double(df.format(total / count));
+
+        } catch (SQLException s) {
+            System.out.println("SQL statement is not executed! " + s.getMessage());
+        }
+        return fee;
     }
 }
